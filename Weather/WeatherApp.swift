b3bb12f44f3572b22969a7500246288af0fd716c
Lucas Araujo <lucas.bordini@hotmail.com>
2024-04-05 17:35:29 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Lucas Araujo on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
